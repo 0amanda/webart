d47d26554effa7bcae959cafa38a7fb537503fe1
0amanda <amand20201@gmail.com>
2023-10-25 20:52:30 -0500

    javascript demo

---
+++ day16/javascriptdemo/main.js
@@ -0,0 +1,31 @@
+console.log("hello world")
+
+let a=2;
+let b=3;
+
+let c = a + b;
+
+let myName = "amanda"
+
+console.log(c);
+console.log(myName);
+
+//for(let i=0; i < 10; i++) {
+  //  console.log(i);
+//}
+
+//store a reference to myButton in JavaScript
+let myButton = document.getElementById("myButton")
+
+//add event listener that listens for when the user clicks on the button
+myButton.addEventListener("click", function(){
+
+    //function body, stuff that should happpen
+console.log("clicked")
+
+//store reference of circle
+let myCircle = document.getElementById("myCircle")
+
+//change background color of circle
+myCircle.style.backgroundColor = "orangered";
+})
